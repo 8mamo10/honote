67fbd35c3e6aeb7921aaab5da34687710f896aaf
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-04-10 11:43:47 +0900

    Add each blog page

---
+++ honote/src/app/blogs/[id]/page.tsx
@@ -0,0 +1,30 @@
+//src/app/page.tsx
+import { hono } from "@/lib/hono";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: Promise<{ id: string }>
+}
+
+export default async function Page({ params }: Props) {
+
+  const { id } = await params;
+  const res = await hono.api.blogs[":id"].$get({
+    param: {
+      id
+    }
+  })
+
+  const blog = await res.json()
+
+  if (!blog) {
+    notFound()
+  }
+
+  return (
+    <div>
+      <h2>{blog.title}</h2>
+      <p>{blog.content}</p>
+    </div>
+  );
+}
