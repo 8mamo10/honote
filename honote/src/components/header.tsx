a9f77c5f44fab7e61c1ab7977ebcc2b8076ab8b1
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-04-18 10:58:56 +0700

    Show button to new post

--- honote/src/components/header.tsx
+++ honote/src/components/header.tsx
@@ -1,5 +1,6 @@
-//src/compoents/header.tsx
+//components/header.tsx
 import { auth, signIn, signOut } from "@/auth";
+import Link from "next/link";
 
 export default async function Header() {
   const session = await auth()
@@ -7,7 +8,9 @@ export default async function Header() {
   return (
     <header className="h-[70px] border-b">
       <div className="container mx-auto h-full flex items-center justify-between px-3">
-        <h1 className="text-[1.5rem] font-bold">🔥 honote</h1>
+        <h1 className="text-[1.5rem] font-bold">
+          <Link href="/">🔥 honote</Link>
+        </h1>
         {!session && <form
           action={async () => {
             "use server"
@@ -16,27 +19,39 @@ export default async function Header() {
         >
           <button
             type="submit"
-            className="bg-yellow-300 py-1 px-3 rounded-full font-bold 
-             hover:bg-yellow-400 hover:shadow-md 
-             transition-all duration-200 ease-in-out 
+            className="bg-yellow-300 py-1 px-3 rounded-full font-bold
+             hover:bg-yellow-400 hover:shadow-md
+             transition-all duration-200 ease-in-out
              active:scale-95"
-          >ログイン</button>
+          >Login</button>
         </form>}
 
-        {session && <form
-          action={async () => {
-            "use server"
-            await signOut()
-          }}
-        >
-          <button
-            type="submit"
-            className="bg-yellow-300 py-1 px-3 rounded-full font-bold 
-             hover:bg-yellow-400 hover:shadow-md 
-             transition-all duration-200 ease-in-out 
-             active:scale-95"
-          >ログアウト</button>
-        </form>}
+        {session &&
+          <div className="flex gap-2 items-center">
+            <button type="button"
+              className="bg-yellow-300 py-1 px-3 rounded-full font-bold
+               hover:bg-yellow-400 hover:shadow-md
+               transition-all duration-200 ease-in-out
+               active:scale-95">
+              <Link href="/blogs/new">
+                +
+              </Link>
+            </button>
+            <form
+              action={async () => {
+                "use server"
+                await signOut()
+              }}
+            >
+              <button
+                type="submit"
+                className="bg-yellow-300 py-1 px-3 rounded-full
+                 ont-bold hover:bg-yellow-400 hover:shadow-md
+                 transition-all duration-200 ease-in-out active:scale-95"
+              >Logout</button>
+            </form>
+          </div>
+        }
       </div>
     </header>
   );
